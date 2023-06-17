1a3ecdee83f8f5c0b2c74d4dcda03698a24e7779
prashantcodehub <113547581+prashantcodehub@users.noreply.github.com>
2023-06-18 00:04:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+/** @format */
+
+let arr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+function PrintDeveloperbyMap() {
+  //Write your code here , just console.log
+  arr.map( (element => {
+        if(element.profession === "developer"){
+            console.log(element);
+        }
+    }))
+
+
+}
+
+function PrintDeveloperbyForEach() {
+  //Write your code here , just console.log
+  arr.forEach(function (arr) {
+        console.log(arr);
+    });
+    
+}
+
+function addData() {
+  //Write your code here, just console.log
+  
+}
+
+function removeAdmin() {
+  //Write your code here, just console.log
+}
+
+function concatenateArray() {
+  //Write your code here, just console.log
+}
